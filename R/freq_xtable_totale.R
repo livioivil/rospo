9413601486b40ff97c9475a22c950b967fb0719a
livio <livio@stat.unipd.it>
2016-03-01 19:02:31 +0100

    greq_xtable_total

--- R/freq_xtable_totale.R
+++ R/freq_xtable_totale.R
@@ -3,9 +3,11 @@
 #'@description as xtable, but put a line between befor the total. Also substitute `Sum` with `Totale`
 #'@param tab a freq table
 #'@param digits as in xtable
+#'@export
 
-freq_xtable_totale <-function(tab,digits = c(0,0,2)){
+freq_xtable_totale <-function(tab,digits = NULL,...){
   rownames(tab)=gsub("Sum","Totale",rownames(tab))
-  xtab=  xtable(tab,digits = digits)
-  print(xtab,hline.after = getOption("xtable.hline.after", c(-1,0,nrow(xtab)-1,nrow(xtab))))
+  if(is.null(digits)) digits=c(0,rep(c(0,2),length.out = ncol(tab)))
+  xtab=  xtable(tab,digits = digits,align =c("l",rep('r',ncol(tab))))
+  print(xtab,hline.after = getOption("xtable.hline.after", c(-1,0,nrow(xtab)-1,nrow(xtab))),...)
 }
